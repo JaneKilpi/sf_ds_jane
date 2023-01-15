bb6e2573513b70aef933d89e732785846ab83340
JaneKilpi <prillumine@gmail.com>
2023-01-15 03:57:47 +0300

    homework_1

---
+++ guess-number-task/game_v2.py
@@ -0,0 +1,58 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число не более чем за 20 попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    min = 1 
+    max = 100 
+    predict_number = np.random.randint(1, 101)  
+
+    while True:
+        count += 1
+        if number > predict_number:
+            min = predict_number
+            predict_number = max - (max - min)//2
+        elif number < predict_number:
+            max = predict_number
+            predict_number = min + (max - min)//2
+        else:
+            break  
+
+    return count
+   
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    random_array = np.random.randint(1, 101, size=(1000))  
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
